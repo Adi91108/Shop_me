45a30bfa63f4e4fec0dc5f2b754d25c1a449fd22
Adi91108 <adityasen911@gmail.com>
2023-07-01 16:39:31 +0530

    chnge

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // myProducts.filter((item)=>item.title.includes(search.value))
 
 // myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
+//we use event listener for loading pages on click
 
 const loginBtn = document.getElementById('login');
 const signBtn = document.getElementById('signup');
